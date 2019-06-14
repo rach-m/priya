36740809a8448e03e8602e3516c7a6bfe8a46c19
Rachel Moskowitz <rachelbmoskowitz@gmail.com>
2019-06-14 13:39:44 -0400

    header mostly done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Navigation/Header"
+import Footer from "./components/Navigation/Footer"
+class App extends React.Component {
+ onResize = () => this.forceUpdate()
 
-function App() {
+
+  render(){
+    window.addEventListener('resize', this.onResize)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+<Header></Header>
+    <div>I am content</div>
+<Footer></Footer>
+</div>
   );
-}
+}}
 
 export default App;
